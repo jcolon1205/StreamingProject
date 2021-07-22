aab2023ed9d2561f4a036d968694cacfd010ef86
jcolon1205 <jcolon1205@gmail.com>
2021-07-22 12:47:58 -0400

    updated login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template,Response
+from flask import Flask,request,render_template,Response
 import cv2
+import pickle
 
 
 app=Flask(__name__)
@@ -19,6 +20,23 @@ def generate_frames():
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
+@app.route('/')
+def loginPage():
+    return render_template('login.html')
+database={'jcolon1205':'123'}
+
+@app.route('/form_login',methods=['POST','GET'])
+def login():
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+	    return render_template('login.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+	         return render_template('index.html',name=name1)
 
 @app.route('/')
 def index():
