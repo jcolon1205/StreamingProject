eb2d945b627f4ffab35e6e5adcbf52b4162b7c18
jcolon1205 <jcolon1205@gmail.com>
2021-07-23 09:39:16 -0400

    added new livestream url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template,Response
+from flask import Flask,request,render_template, Response
 import cv2
 
 
@@ -18,7 +18,7 @@ def generate_frames():
             frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
